e63417457c8289540c5be3eab204e540a88e7938
Unknown <martin@klec.cz>
2019-01-21 08:25:56 +0100

    Correction of the last commit

---
+++ comp/src/src/params.cpp
@@ -0,0 +1,29 @@
+#include "zsk.h"
+
+const vector<vector<double>> etavsq	={	{439.901,679.738}		,
+        {679.738,4834.49}	}	;
+const double X0	=	2744.82968313331		;
+const double Y0	=	18982.6356951142		;
+
+const double xregconst	=	2252.07		;
+const double yregconst	=	12852.2		;
+const double xregx	=	1.23574		;
+const double yregx	=	2.24683		;
+const double xregy	=	-0.15202		;
+const double yregy	=	0		;
+
+const double spot0	=	7.77		;
+
+
+
+
+const double qcoef	=	0.00974		;
+const double varsigma	=	0.01		;
+const vector<double> f0	={	7.81	,
+        7.87	,
+        7.97	}	;
+
+const double rf	=	0.01		;
+const double bsc	=	0.678		;
+const double bsl	=	-0.00964		;
+const double bsq	=	0.00234		;
